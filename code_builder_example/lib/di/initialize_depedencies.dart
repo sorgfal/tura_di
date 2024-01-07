5295e70caf9986a98dc828a5d1a193a96bb261c5
d.kolyasnikov <d.kolyasnikov@ddxfitness.ru>
2024-01-07 23:57:59 +0500

    example fill

--- code_builder_example/lib/di/initialize_depedencies.dart
+++ code_builder_example/lib/di/initialize_depedencies.dart
@@ -1,6 +1,5 @@
+import 'package:code_builder_example/di/initialize_depedencies.itz.dart';
 import 'package:generator/annotation.dart';
 
 @turaCon
 class InitializeDepedencies {}
-
-main() {}
---
+++ code_builder_example/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:code_builder_example/di/initialize_depedencies.itz.dart';
+
+main() {
+  final box = TuraContainerBox();
+  final rep = box.getNewsRepository;
+}
